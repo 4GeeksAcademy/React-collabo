3fbeeccacde28e6f000ac8bb26a664dd8fd4cecb
PixelJotape <gabopporrino@gmail.com>
2026-03-02 23:47:15 +0000

    NAVBAR & JUMBOTRON FINISHED

---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Jumbotron = () => {
+    return (
+        <div className="bg-light p-5 mb-4 rounded-3">
+            <h1 className="display-4 fw-bold mb-3">A Warn Welcome!</h1>
+            <p className="col-md-12 fs-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quisquam dolor nemo corrupti dolorem, facere adipisci tempora esse rerum repellendus natus voluptatibus qui totam beatae a neque perspiciatis. Fugiat, quia?
+            </p>
+            <button type="button" class="btn btn-primary mt-3">Call to action!</button>
+        </div>
+        );
+};
+
+export default Jumbotron;
